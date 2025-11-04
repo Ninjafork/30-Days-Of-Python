b1672cb573b11d072c4d7d864d3ac4acb963fe78
Will Bennett <108555426+Ninjafork@users.noreply.github.com>
2025-11-04 14:12:20 -0800

    Update variables.py

--- day_02/variables.py
+++ day_02/variables.py
@@ -1,1 +1,14 @@
+#Day 2: 30 Days of python
+first_name = 'Will'
+Last_name = 'Bennett'
+Full_Name = ('first_name, last_name')
+country = 'USA'
+age = 37
+year = 1988
+is_true = 'Yur'
+is_light_on = 'whatever'
+car, engine, rpm_max = 'Mazda', 'Wankel', 9500
+
+
+
 
