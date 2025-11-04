aeb280ae43ffcbebcd18c4cb7739214d7bcef445
Will Bennett <108555426+Ninjafork@users.noreply.github.com>
2025-11-04 15:20:30 -0800

    Update variables.py

--- day_02/variables.py
+++ day_02/variables.py
@@ -1,6 +1,6 @@
 #Day 2: 30 Days of python
-first_name = 'Will'
-Last_name = 'Bennett'
+first_name = Will
+last_name = 'Bennett'
 Full_Name = ('first_name, last_name')
 country = 'USA'
 age = 37
@@ -9,6 +9,9 @@ is_true = 'Yur'
 is_light_on = 'whatever'
 car, engine, rpm_max = 'Mazda', 'Wankel', 9500
 
+#Check the data type
+type([$first_name, last_name, Full_Name, country, age, year, is_true, is_light_on, car, engine, rpm_max])
+len(first_name)
 
 
 
